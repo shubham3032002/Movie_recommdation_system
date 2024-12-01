464e888e257e9ed2d434c4c42e48afbe0983351b
shubham mokal <shubham@gmail.com>
2024-12-02 00:06:32 +0530

    file str completed

---
+++ src/Recommand_system/constants/__init__.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+GONFIG_FILE_PATH =Path("config/config.yaml")
+PARAMS_FILE_PATH =Path("params.yaml")
---
+++ src/Recommand_system/utils/common.py
@@ -0,0 +1,136 @@
+import os
+import sys
+from pathlib import Path
+import numpy as np
+import dill
+import yaml
+from box.exceptions import BoxValueError
+from pandas import DataFrame
+from ensure import ensure_annotations
+from src.Recommand_system.Exception import MyException
+from src.Recommand_system import logger
+
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> dict:
+    """reads yaml file and returns
+
+    Args:
+        path_to_yaml (str): path like input
+
+    Raises:
+        ValueError: if yaml file is empty
+        e: empty file
+
+    Returns:
+        ConfigBox: ConfigBox type
+    """
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return content
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise e
+    
+    
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
+    """create list of directories
+
+    Args:
+        path_to_directories (list): list of path of directories
+        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
+    """
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"created directory at: {path}")    
+
+@ensure_annotations
+def write_yaml_file(file_path: str, content: object, replace: bool = False) -> None:
+    try:
+        if replace:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "w") as file:
+            yaml.dump(content, file)
+    except Exception as e:
+        raise MyException(e, sys) from e
+
+@ensure_annotations
+def load_object(file_path: str) -> object:
+    """
+    Returns model/object from project directory.
+    file_path: str location of file to load
+    return: Model/Obj
+    """
+    try:
+        with open(file_path, "rb") as file_obj:
+            obj = dill.load(file_obj)
+        return obj
+    except Exception as e:
+        raise MyException(e, sys) from e
+@ensure_annotations
+def save_numpy_array_data(file_path: str, array: np.array):
+    """
+    Save numpy array data to file
+    file_path: str location of file to save
+    array: np.array data to save
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise MyException(e, sys) from e
+
+@ensure_annotations
+def load_numpy_array_data(file_path: str) -> np.array:
+    """
+    load numpy array data from file
+    file_path: str location of file to load
+    return: np.array data loaded
+    """
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise MyException(e, sys) from e
+
+@ensure_annotations
+def save_object(file_path: str, obj: object) -> None:
+    logger.info("Entered the save_object method of utils")
+
+    try:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+
+        logger.info("Exited the save_object method of utils")
+
+    except Exception as e:
+        raise MyException(e, sys) from e
+
+
+# def drop_columns(df: DataFrame, cols: list)-> DataFrame:
+
+#     """
+#     drop the columns form a pandas DataFrame
+#     df: pandas DataFrame
+#     cols: list of columns to be dropped
+#     """
+#     logging.info("Entered drop_columns methon of utils")
+
+#     try:
+#         df = df.drop(columns=cols, axis=1)
+
+#         logging.info("Exited the drop_columns method of utils")
+        
+#         return df
+#     except Exception as e:
+#         raise MyException(e, sys) from e
